45f4780036981b32b277289ef4f8c7078affb24e
Soumya Jain <jainsoumya647@gmail.com>
2020-07-05 04:43:28 +0530

    Binary Tree Traversal | Leetcode July

---
+++ Binary Tree Level Order Traversal II/main.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  Binary Tree Level Order Traversal II
+//
+//  Created by Soumya Jain on 05/07/20.
+//  Copyright © 2020 Soumya Jain. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+class Solution {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        var result = [[Int]]()
+        guard let node = root else {
+            return result
+        }
+        var parent = [node]
+        while !parent.isEmpty {
+            var size = parent.count
+            var array = [Int]()
+            while(size != 0) {
+                guard let currentNode = parent.first else { break }
+                array.append(currentNode.val)
+                parent.removeFirst()
+                if let left = currentNode.left {
+                    parent.append(left)
+                }
+                if let right = currentNode.right {
+                    parent.append(right)
+                }
+                size -= 1
+            }
+            
+            result.append(array)
+        }
+        
+        result = result.reversed()
+        return result
+    }
+}
+
